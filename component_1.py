3cff83f1ce3b8a65a99b4937bb8ef492a9b582dd
podchasova11 <pod4asova20@gmail.com>
2026-01-31 16:13:05 +0300

    sfasdff

---
+++ component_1.py
@@ -0,0 +1,25 @@
+
+age = 35
+name = "Mila"
+is_student = True
+temperature = 26.6
+
+print([age, name, is_student, temperature])
+
+x = 10
+x = 20
+
+a, b = 15, 20
+print(a + b)
+print(b - a)
+print(a * b)
+print(a / b)
+print(a ** b)
+print(a % b)
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
