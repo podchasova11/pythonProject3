a6461903fc7a4d475683b3f9a3f9c3ca4852e6cd
podchasova11 <pod4asova20@gmail.com>
2026-02-05 17:12:15 +0300

    added

--- component_1.py
+++ component_1.py
@@ -1,28 +1,35 @@
-
-age = 35
-name = "Mila"
-is_student = True
-temperature = 26.6
-
-print([age, name, is_student, temperature])
-
-
-x = 10
-x = 20
+#
+# age = 35
+# name = "Mila"
+# is_student = True
+# temperature = 26.6
+#
+# print([age, name, is_student, temperature])
 
 
-a, b = 15, 20
-print(a + b)
-print(b - a)
-print(a * b)
-print(a / b)
-print(a ** b)
-print(a % b)
+# x = 10
+# x = 20
+#
+#
+# a, b = 15, 20
+# print(a + b)
+# print(b - a)
+# print(a * b)
+# print(a / b)
+# print(a ** b)
+# print(a % b)
+#
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
 
-print(a == b)
-print(a != b)
-print(a > b)
-print(a < b)
-print(a >= b)
-print(a <= b)
+text = "Python - замечательный язык программирования"
 
+contains_python = "Python" in text  # True, так как "Python" содержится в строке
+contains_java = "Java" in text  # False, так как "Java" не содержится в строке
+
+print(contains_python)
+print(contains_java)
